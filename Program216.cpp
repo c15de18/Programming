6fd17e91c6817ca4ed100ad82558964888ac6e76
c15de18 <137049325+c15de18@users.noreply.github.com>
2026-02-10 14:38:39 +0530

    Add files via upload

---
+++ Program216.cpp
@@ -0,0 +1,251 @@
+#include<iostream>
+using namespace std;
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+}NODE, *PNODE, **PPNODE;
+
+class SinglyLL
+{
+    private:
+        PNODE first;
+        int Count;
+
+    public:
+        SinglyLL();
+        void InsertFirst(int no);
+        void InsertLast(int no);
+        void InsertAtPos(int no, int ipos);
+        void DeleteFirst();
+        void DeleteLast();
+        void DeleteAtPos(int ipos);
+        void Display();
+        int CountNode();
+};
+
+SinglyLL::SinglyLL()
+{
+    cout<<"Inside constructor\n";
+    first = NULL;
+    Count = 0;
+}
+
+void SinglyLL :: InsertFirst(int no)
+{
+    PNODE newn = NULL;
+
+    newn = new NODE;
+    newn->data = no;
+    newn->next = NULL;
+
+    if(first == NULL)
+    {
+        first= newn;
+    }
+    else
+    {
+        newn->next = first;
+        first = newn;
+    }
+    Count++;
+
+}
+
+void SinglyLL :: InsertLast(int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = first;
+
+    newn = new NODE;
+    newn->data = no;
+    newn->next = NULL;
+
+    if(first == NULL)
+    {
+        first  = newn;
+    }
+    else
+    {
+        while(temp->next != NULL)
+        {
+            temp= temp->next;
+        }
+        temp->next = newn;
+    }
+    Count++;
+}
+
+void SinglyLL :: Display()
+{
+    PNODE temp = first;
+
+    cout<<"Elements of linked list are : \n";
+
+    while (temp != NULL)
+    {
+        cout<<"| "<<temp->data<<" | -> ";
+        temp = temp->next;
+    }
+    cout<<"NULL \n";
+    
+}
+
+int SinglyLL :: CountNode()
+{
+    return Count;
+}
+
+void SinglyLL :: DeleteFirst()
+{
+    if(first == NULL)
+    {
+        return;
+    }
+    else if(first->next == NULL)
+    {
+        delete first;
+        first = NULL;
+    }
+    else 
+    {
+        PNODE temp = first;
+        first = first->next;
+        delete temp;
+    }
+    Count--;
+}
+
+void SinglyLL :: DeleteLast()
+{
+    if(first == NULL)
+    {
+        return;
+    }
+    else if(first->next == NULL)
+    {
+        delete first;
+        first = NULL;
+    }
+    else
+    {
+        PNODE temp = first;
+
+        while(temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        delete temp->next;
+        temp->next = NULL;
+    }
+    Count--;
+}
+
+void SinglyLL :: InsertAtPos(int no,int ipos)
+{
+    if((ipos < 1) || (ipos > Count+1))
+    {
+        cout<<"Invalid position"<<endl;
+        return;
+    }
+    else if(ipos == 1)
+    {
+        InsertFirst(no);
+    }
+    else if(ipos == Count)
+    {
+        InsertLast(no);
+    }
+    else
+    {
+        PNODE newn = new NODE;
+        newn->data = no;
+        newn->next = NULL;
+
+        PNODE temp = first;
+        for(int i = 1; i < ipos-1; i++)
+        {
+            temp = temp->next;
+        }
+        newn->next = temp->next;
+        temp->next = newn;
+        Count++;
+    }
+}
+
+void SinglyLL::DeleteAtPos(int ipos)
+{
+    if((ipos < 1) || (ipos > Count+1))
+    {
+        cout<<"Invalid position"<<endl;
+        return;
+    }
+    else if(ipos == 1)
+    {
+        DeleteFirst();
+    }
+    else if(ipos == Count)
+    {
+        DeleteLast();
+    }
+    else
+    {
+        PNODE temp = first;
+        for(int i = 1; i < ipos-1; i++)
+        {
+            temp = temp->next;
+        }
+        PNODE targetednode = temp->next;
+        temp->next = temp->next->next;
+        delete targetednode;
+
+        Count--;
+    }
+}
+
+int main()
+{
+    SinglyLL obj();
+    int iRet = 0;
+
+
+    obj.InsertFirst(111);
+    obj.InsertFirst(101);
+    obj.InsertFirst(51);
+    obj.InsertFirst(21);
+    obj.InsertFirst(11);
+
+    obj.InsertLast(121);
+    obj.InsertLast(151);
+    obj.InsertLast(201);
+
+    
+    obj.Display();
+    iRet = obj.CountNode();
+    cout<<"Number of elements in the linked list are : "<<iRet<<"\n";
+
+    obj.InsertAtPos(105,5);
+    obj.Display();
+
+    iRet = obj.CountNode();
+    cout<<"Number of elements in the linked list are : "<<iRet<<"\n";
+
+    obj.DeleteAtPos(5);
+
+    obj.Display();
+
+    iRet = obj.CountNode();
+    cout<<"Number of elements in the linked list are : "<<iRet<<"\n";
+
+
+    obj.DeleteFirst();
+    obj.DeleteLast();
+
+    obj.Display();
+
+    iRet = obj.CountNode();
+    cout<<"Number of elements in the linked list are : "<<iRet<<"\n";
+
+    return 0;
+}
